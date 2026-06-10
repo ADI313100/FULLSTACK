06ec086979b200faad2d5d2976f7e745d0a0c4e6
Adithya <shankarradhakrishnan01@gmail.com>
2026-06-10 11:50:29 +0530

    JS

---
+++ .vscode/Basic.js
@@ -0,0 +1,32 @@
+const first = 1;
+const second = 2;
+console.log("first: ", first);
+console.log("second: ", second);
+const Fruits = ["Apple","Banana","Grapes"];
+console.log("Fruits[0]:");
+console.log(Fruits[0]);
+console.log("Fruits[1]:");
+console.log(Fruits[1]);
+console.log("Fruits[2]:");
+console.log(Fruits[2]);
+const arr = [1,"name",false,2,4]
+console.log(arr);
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+const fourth = document.getElementById("ptag");
+fourth.innerHTML = "This is a paragraph";
+
+
+function newfunction(){
+
+}
+const input = document.getElementById("eventListener");
+const output = document.getElementById("ptag");
+input.addEventListener("input", function(){
+    output.innerHTML = input.value;
+});
+const newvar = document.createElement("hi");
+newvar.textcontent = "New paragraph";
+document.body.appendChild(newvar);
+newvar.removeChild();
